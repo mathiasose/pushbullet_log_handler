d1e81bd92778c3be2b36a66e33416c3e25a051f9
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-11-10 22:48:33 +0100

    rename to PushbulletLogHandler and add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pushbullet_log_handler',
+      version='0.1',
+      description='Pushbullet Logging Handler',
+      author='Mathias Ose, Øyvind Robertsen',
+      author_email='m@thiaso.se, me@oyvindrobertsen.com',
+      url='http://github.com/mathiasose/pushbullet_logger',
+      py_modules=['PushbulletLogHandler'],
+      )
